f5c6a48bfa7a0e49a88b0c7c7828682071061926
Oskar Friberg <oskf@steria.no>
2013-10-31 21:13:50 +0100

    support pushStr/start

--- app/controllers/ReceivePush.scala
+++ app/controllers/ReceivePush.scala
@@ -14,7 +14,8 @@ object ReceivePush extends Controller {
   def data = Action { request =>
     println("RECEIVER: Got new batch of data to receivePush:")
     Thread.sleep(100)
-    //request.body.asJson.map{x =>println(x)}
+    println(request.body)
+    //request.body.asJson.map{x =>println("some data:"+x.toString.substring(100)+ "some")}
     println("RECEIVER: End batch of data")
     Ok("")
   }
