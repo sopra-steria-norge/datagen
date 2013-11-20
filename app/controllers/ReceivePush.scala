feb71629652dc0a526eab6cdc7e786078fd6cdf4
Oskar Friberg <oskf@steria.no>
2013-11-20 09:46:38 +0100

    remove body println

--- app/controllers/ReceivePush.scala
+++ app/controllers/ReceivePush.scala
@@ -14,7 +14,7 @@ object ReceivePush extends Controller {
   def data = Action { request =>
     println("RECEIVER: Got new batch of data to receivePush:")
     Thread.sleep(100)
-    println(request.body)
+    //println(request.body)
     //request.body.asJson.map{x =>println("some data:"+x.toString.substring(100)+ "some")}
     println("RECEIVER: End batch of data")
     Ok("")
