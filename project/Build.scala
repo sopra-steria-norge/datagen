cb5660a2ec648497d46b27b388b7cedd329f58e4
oskar-friberg <oskf@steria.no>
2014-01-13 12:37:41 +0100

    add elasticsearch-cloud-aws dependency

--- project/Build.scala
+++ project/Build.scala
@@ -6,6 +6,8 @@ object ApplicationBuild extends Build {
 
   val appName         = "datagen"
   val appVersion      = "1.0-SNAPSHOT"
+    
+    val esVersion = "0.90.9"
 
   val appDependencies = Seq(
     // Add your project dependencies here,
@@ -16,7 +18,8 @@ object ApplicationBuild extends Build {
   "com.typesafe" % "config" % "1.0.2",
   "org.streum" %% "configrity-core" % "1.0.0",
   "com.fasterxml.jackson.module" %% "jackson-module-scala" % "2.1.3",
-  "org.elasticsearch" % "elasticsearch" % "0.90.9"
+  "org.elasticsearch" % "elasticsearch" % esVersion,
+  "org.elasticsearch" % "elasticsearch-cloud-aws" % "0.16.0"
 
   )
   
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -2,7 +2,10 @@
 logLevel := Level.Warn
 
 // The Typesafe repository 
+resolvers += "Sonatype" at "http://oss.sonatype.org/content/repositories/releases"
+
 resolvers += "Typesafe repository" at "http://repo.typesafe.com/typesafe/releases/"
+
 
 // Use the Play sbt plugin for Play projects
 addSbtPlugin("com.typesafe.play" % "sbt-plugin" % System.getProperty("play.version"))
